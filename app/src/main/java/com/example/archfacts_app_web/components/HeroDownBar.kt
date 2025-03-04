5d68ec089965f2366a1622eb2b063403f2b12bcc
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2025-03-04 14:35:06 -0300

    adding images

---
+++ app/src/main/java/com/example/archfacts_app_web/components/HeroDownBar.kt
@@ -0,0 +1,15 @@
+package com.example.archfacts_app_web.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun HeroDownBar() {
+
+}
+
+@Preview
+@Composable
+fun HeroDownBarPreview() {
+    
+}
