16204704a504a1e9eec08e4affad07708ce43bb0
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2025-03-11 13:02:35 -0300

    redesigning the container

--- app/src/main/java/com/example/archfacts_app_web/components/IconTextContainer.kt
+++ app/src/main/java/com/example/archfacts_app_web/components/IconTextContainer.kt
@@ -4,16 +4,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -33,29 +38,57 @@ fun IconTextLayout(icon: Painter, title: String, desc: String) {
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp), verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Image(
             painter = icon,
             contentDescription = title,
-            modifier = Modifier.size(24.dp)
+            modifier = Modifier.size(48.dp)
         )
-    }
-    Text(
-        text = title,
-        fontSize = 12.sp,
-        fontFamily = Poppins,
-        fontWeight = FontWeight.Normal,
-        color = Color.White,
-        maxLines = 1
+        Text(
+            text = title,
+            fontSize = 24.sp,
+            fontFamily = Poppins,
+            fontWeight = FontWeight.Normal,
+            color = Color.White,
+            maxLines = 1,
+            modifier = Modifier.padding(top = 8.dp)
 
-    )
-    Text(
-        text = desc,
-        fontSize = 12.sp,
-        fontFamily = Poppins,
-        fontWeight = FontWeight.Light,
-        color = Color.White,
-    )
+        )
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.seta_esquerda),
+                contentDescription = "Seta direita",
+                modifier = Modifier.size(32.dp)
+            )
+
+            Text(
+                text = desc,
+                fontSize = 16.sp,
+                fontFamily = Poppins,
+                fontWeight = FontWeight.Light,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(vertical = 5.dp)
+                    .weight(1f),
+                textAlign = TextAlign.Center
+
+            )
+
+            Image(
+                painter = painterResource(id = R.drawable.seta_direita),
+                contentDescription = "Seta esquerda",
+                modifier = Modifier
+                    .size(32.dp)
+            )
+        }
+    }
 }
 
 @Composable
@@ -66,21 +99,21 @@ fun IconTextContainer() {
             title = "Desorganização",
             desc = "Um estudo conduzido pela Sebrae aponta que cerca de 79% das empresas brasileiras enfrentam sérios problemas de gestão, o que impactam no seu crescimento e sustentabilidade, portanto afetando o negócio a longo prazo."
         ),
-        IconTextElement(
-            icon = painterResource(id = R.drawable.user_icon),
-            title = "Comunicação com os clientes",
-            desc = "Falhas na comunicação com o cliente se tornam um empecilho no seu negócio, gerando desgaste na relação, atraso na entrega do produto e no pior dos casos, erros pela má comunicação. No fim gerando uma perda para o negócio."
-        ),
-        IconTextElement(
-            icon = painterResource(id = R.drawable.number_one_icon),
-            title = "Priorização de atividades",
-            desc = "Uma pesquisa realizada pela consultoria Falconi revelou a fragilidade na gestão das médias empresas no Brasil. Somente 10% das médias empresas tem planejamentos de longo prazo, a falta de planejamento gera queda na atividade econômica."
-        ),
-        IconTextElement(
-            icon = painterResource(id = R.drawable.fluxo_caixa),
-            title = "Controle do fluxo de caixa",
-            desc = "Sem o controle do fluxo de caixa a sua empresa fica suscetível e imprevistos financeiros podem ser fatais. Um estudo realizado pelo IBGE em 2022 revela que 48% das empresas brasileiras fecham em até 3 anos por conta da má gestão financeira."
-        )
+//        IconTextElement(
+//            icon = painterResource(id = R.drawable.user_icon),
+//            title = "Comunicação com os clientes",
+//            desc = "Falhas na comunicação com o cliente se tornam um empecilho no seu negócio, gerando desgaste na relação, atraso na entrega do produto e no pior dos casos, erros pela má comunicação. No fim gerando uma perda para o negócio."
+//        ),
+//        IconTextElement(
+//            icon = painterResource(id = R.drawable.number_one_icon),
+//            title = "Priorização de atividades",
+//            desc = "Uma pesquisa realizada pela consultoria Falconi revelou a fragilidade na gestão das médias empresas no Brasil. Somente 10% das médias empresas tem planejamentos de longo prazo, a falta de planejamento gera queda na atividade econômica."
+//        ),
+//        IconTextElement(
+//            icon = painterResource(id = R.drawable.fluxo_caixa),
+//            title = "Controle do fluxo de caixa",
+//            desc = "Sem o controle do fluxo de caixa a sua empresa fica suscetível e imprevistos financeiros podem ser fatais. Um estudo realizado pelo IBGE em 2022 revela que 48% das empresas brasileiras fecham em até 3 anos por conta da má gestão financeira."
+//        )
     )
 
     Column(
