f151ee6877a90a778650909e45bf961fe02074f9
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2025-03-13 17:14:39 -0300

    Creating the structure of forms component

--- app/src/main/java/com/example/archfacts_app_web/components/TitleDescContainer.kt
+++ app/src/main/java/com/example/archfacts_app_web/components/TitleDescContainer.kt
@@ -9,11 +9,13 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
@@ -25,7 +27,7 @@ data class TitleStyle(
     val fontSize: TextUnit = 24.sp,
     val color: Color = Color.Black,
     val fontFamily: FontFamily = Poppins,
-    val fontWeight: FontWeight = FontWeight.Normal
+    val fontWeight: FontWeight = FontWeight.Normal,
 )
 
 data class DescStyle(
--- app/src/main/java/com/example/archfacts_app_web/screens/ContactSection.kt
+++ app/src/main/java/com/example/archfacts_app_web/screens/ContactSection.kt
@@ -1,4 +1,30 @@
 package com.example.archfacts_app_web.screens
 
-class ContactSection {
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.archfacts_app_web.components.DescStyle
+import com.example.archfacts_app_web.components.TitleDescContainer
+import com.example.archfacts_app_web.components.TitleStyle
+
+@Composable
+fun ContactSection() {
+    Column {
+        TitleDescContainer(
+            200.dp, "Entre em contato", "Caso tenha dúvidas," +
+                    " preencha o formulário e entre em contato conosco!", titleStyle = TitleStyle(
+                fontSize = 24.sp, fontWeight = FontWeight.Bold
+            ),
+            descStyle = DescStyle(fontSize = 16.sp, fontWeight = FontWeight.Normal)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ContactSectionPreview() {
+    ContactSection()
 }
