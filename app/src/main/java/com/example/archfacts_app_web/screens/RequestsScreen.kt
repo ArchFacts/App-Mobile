2da452cc47be0438d2845791473660f22ea81122
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2025-04-09 12:53:50 -0300

    adding counter

---
+++ app/src/main/java/com/example/archfacts_app_web/screens/RequestsScreen.kt
@@ -0,0 +1,16 @@
+package com.example.archfacts_app_web.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun RequestsScreen(modifier: Modifier = Modifier) {
+
+}
+
+@Preview
+@Composable
+fun RequestsScreenPreview() {
+    RequestsScreen()
+}
