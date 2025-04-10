a7b0babad3008136009cc0161fd2615d123f46b2
LuisGustavoDAlmeida <luis.almeida@sptech.school>
2025-04-10 14:32:59 -0300

    finishing RequestsScreen

--- app/src/main/java/com/example/archfacts_app_web/components/NavigationBar.kt
+++ app/src/main/java/com/example/archfacts_app_web/components/NavigationBar.kt
@@ -3,12 +3,12 @@ package com.example.archfacts_app_web.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -17,6 +17,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.rotate
@@ -26,7 +27,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.min
 import androidx.compose.ui.unit.sp
 import com.example.archfacts_app_web.ui.theme.ArchOrange
 import com.example.archfacts_app_web.ui.theme.Poppins
@@ -46,7 +46,11 @@ fun NavigationBar(text: String, color: Color) {
             .clip(RoundedCornerShape(8.dp)),
         color = Color.White,
     ) {
-        Row(modifier = Modifier.height(50.dp), horizontalArrangement = Arrangement.Center) {
+        Row(
+            modifier = Modifier.height(50.dp),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
             Column(
                 verticalArrangement = Arrangement.Center,
                 modifier = Modifier
@@ -70,8 +74,10 @@ fun NavigationBar(text: String, color: Color) {
                 fontFamily = Poppins,
                 fontWeight = FontWeight.Medium,
                 fontSize = 32.sp,
-                textAlign = TextAlign.Center,
-                modifier = Modifier.fillMaxWidth()
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 32.dp)
             )
         }
     }
