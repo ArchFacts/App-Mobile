80f8b3c2cd081de3ba3357a06fe3e10fed18731e
diegodcampos <diego.arruda@sptech.school>
2025-04-24 20:14:41 -0300

    Feat: Creating DataConfirmation page and fixing gramatical issues

---
+++ app/src/main/java/com/example/archfacts_app_web/screens/DataConfirmation.kt
@@ -0,0 +1,154 @@
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.archfacts_app_web.components.CustomButton
+import com.example.archfacts_app_web.components.FooterProposal
+import com.example.archfacts_app_web.ui.theme.ArchOrange
+import com.example.archfacts_app_web.ui.theme.Poppins
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Confirmation() {
+    val solicitanteOptions = listOf("Nome", "Telefone", "Email")
+    val servicoOptions = listOf("Serviço A", "Serviço B", "Serviço C")
+
+    var selectedSolicitante by remember { mutableStateOf(solicitanteOptions[0]) }
+    var expandedSolicitante by remember { mutableStateOf(false) }
+
+    var selectedServico by remember { mutableStateOf(servicoOptions[0]) }
+    var expandedServico by remember { mutableStateOf(false) }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Spacer(modifier = Modifier.weight(0.2f))
+
+        Text(
+            text = "Confirmação dos dados",
+            fontSize = 34.sp,
+            fontWeight = FontWeight.Bold,
+            fontFamily = Poppins,
+            color = Color(0xFF0E0C19),
+            textAlign = TextAlign.Center,
+        )
+
+        Text(
+            text = " Confirme os dados antes de enviar a solicitação para {negocio.nome}",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.SemiBold,
+            textAlign = TextAlign.Center,
+            fontFamily = Poppins,
+            modifier = Modifier.padding(vertical = 20.dp)
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+
+        ExposedDropdownMenuBox(
+            expanded = expandedServico,
+            onExpandedChange = { expandedServico = !expandedServico },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp)
+        ) {
+            TextField(
+                value = selectedSolicitante,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text("Dados do solicitante") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedSolicitante) },
+                modifier = Modifier.menuAnchor().fillMaxWidth()
+            )
+            ExposedDropdownMenu(
+                expanded = expandedSolicitante,
+                onDismissRequest = { expandedSolicitante = false }
+            ) {
+                solicitanteOptions.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(option) },
+                        onClick = {
+                            selectedSolicitante = option
+                            expandedSolicitante = false
+                        }
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(20.dp))
+
+        ExposedDropdownMenuBox(
+            expanded = expandedServico,
+            onExpandedChange = { expandedServico = !expandedServico },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 25.dp)
+        ) {
+            TextField(
+                value = selectedServico,
+                onValueChange = {},
+                readOnly = true,
+                label = { Text("Serviços Escolhidos") },
+                trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expandedServico) },
+                modifier = Modifier.menuAnchor().fillMaxWidth()
+            )
+            ExposedDropdownMenu(
+                expanded = expandedServico,
+                onDismissRequest = { expandedServico = false }
+            ) {
+                servicoOptions.forEach { option ->
+                    DropdownMenuItem(
+                        text = { Text(option) },
+                        onClick = {
+                            selectedServico = option
+                            expandedServico = false
+                        }
+                    )
+                }
+            }
+        }
+
+        Spacer(modifier = Modifier.height(35.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth().padding(horizontal = 25.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            CustomButton(
+                "Voltar",
+                { println("Clicou em Voltar") },
+                165.dp,
+                35.dp,
+                Color(0xFF033E8C)
+            )
+
+            CustomButton(
+                "Continuar",
+                { println("Clicou em Continuar") },
+                165.dp,
+                35.dp,
+                ArchOrange
+            )
+        }
+
+        Spacer(modifier = Modifier.weight(0.2f))
+
+        FooterProposal(activeIndex = 0, modifier = Modifier.fillMaxWidth())
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ConfirmationPreview() {
+    Confirmation()
+}
